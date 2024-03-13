311d8b62071d03e165721c0e03d4ea23b2a6b2dd
Artur Das <a.daschevici@gmail.com>
2024-03-13 16:08:03 +0100

    Update openai package dependencies to get rid of deprecation warnings

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import streamlit as st
 from pydantic import Field, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from PyPDF2 import PdfReader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file="config.env", env_file_encoding="utf-8")
     hf_access_token: SecretStr = Field(alias="HUGGINGFACEHUB_API_TOKEN")
     openai_api_key: SecretStr = Field(alias="OPENAI_API_KEY")
+
 
 def get_text_from_pdf(pdf_docs):
     raw_text = ""
@@ -16,6 +19,21 @@ def get_text_from_pdf(pdf_docs):
         for page in pdf_reader.pages:
             raw_text += page.extract_text()
     return raw_text
+
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+    )
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    vector_store = {}
+    for idx, chunk in enumerate(text_chunks):
+        vector_store[idx] = chunk
+    return vector_store
+
 
 def main():
     settings = Settings()
@@ -33,8 +51,14 @@ def main():
         )
         if st.button("Process"):
             with st.spinner("Processing..."):
+                # get raw content from pdf
                 raw_text = get_text_from_pdf(pdf_docs)
-                st.write(raw_text)
+
+                # split the text into chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store for each chunk
+                vector_store = get_vector_store(text_chunks)
 
 
 if __name__ == "__main__":
