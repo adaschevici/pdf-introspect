3bf7b90799368e2807aceae4836666289a0e59f8
Artur Das <a.daschevici@gmail.com>
2024-03-13 15:11:33 +0100

    Add config and include pydantic for env management

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file="config.env", env_file_encoding="utf-8")
 
 def main():
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:", layout="wide")
@@ -9,6 +13,7 @@ def main():
     with st.sidebar:
         st.subheader("Your documents")
         st.file_uploader("Upload some PDFs and click process", type="pdf", accept_multiple_files=True)
+        st.button("Process")
 
 if __name__ == '__main__':
     main()
